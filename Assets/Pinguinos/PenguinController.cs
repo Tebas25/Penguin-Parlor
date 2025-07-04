1a25cd8a0b29cf3ddcd80400320e157ade92ac00
tom-ayu <tomas.ontaneda@udla.edu.ec>
2025-07-03 20:43:27 -0500

    penguin re idle/walk, anim transitions, navmesh agent/mesh fixes, mesas disposition/colliders, addon hats

--- Assets/Pinguinos/PenguinController.cs
+++ Assets/Pinguinos/PenguinController.cs
@@ -5,19 +5,29 @@ public class PenguinController : MonoBehaviour
 {
     public Camera cam;
     public NavMeshAgent player;
+    private Animator animator;
 
-    // Update is called once per frame
+    void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+    }
+
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-            // RaycastHit hitpoint;
             if (Physics.Raycast(ray, out RaycastHit hitpoint))
             {
                 player.SetDestination(hitpoint.point);
             }
         }
-        
+
+        // Cambio animación por la velocidad del agente
+        float speed = player.velocity.magnitude;
+        if (animator != null)
+        {
+            animator.SetFloat("speed", speed);
+        }
     }
 }
